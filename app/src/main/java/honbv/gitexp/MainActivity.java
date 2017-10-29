60e4e9b57f735a23776f71672a680148503fd1de
ParkjinBum <honbv2@naver.com>
2017-10-30 02:22:21 +0900

    commit???

--- app/src/main/java/honbv/gitexp/MainActivity.java
+++ app/src/main/java/honbv/gitexp/MainActivity.java
@@ -10,5 +10,6 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         // 노트북에서 작성 Git
+        // commit???
     }
 }
