9c69eed94b400db296ede840b125d08e82169ed4
ParkJinBum <honbv2@naver.com>
2017-10-30 01:18:54 +0900

    Initial commit

---
+++ app/src/main/java/honbv/gitexp/MainActivity.java
@@ -0,0 +1,14 @@
+package honbv.gitexp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        // 노트북에서 작성 Git
+    }
+}
